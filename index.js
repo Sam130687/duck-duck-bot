8b42a73122289bebdc377dd80268d42067f1f30f
Sam130687 <58412574+Sam130687@users.noreply.github.com>
2021-11-12 13:30:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const sample = require('lodash/sample');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Send mе any sticker'));
+bot.start((ctx) => ctx.reply("Let's start!"));
 
 bot.on('sticker', (ctx) => {
     const { sticker } = ctx.update.message;
@@ -18,8 +18,62 @@ bot.on('sticker', (ctx) => {
         });
 });
 
+const keyboard = [
+    [
+      {
+        text: 'Хочу кота', // текст на кнопке
+        callback_data: 'moreKeks' // данные для обработчика событий
+      }
+    ],
+    [
+      {
+        text: 'Хочу песика',
+        callback_data: 'morePes'
+      }
+    ],
+    [
+      {
+        text: 'Хочу проходить курсы',
+        url: 'https://htmlacademy.ru/courses' //внешняя ссылка
+      }
+    ]
+  ];
+
+// Обработчик нажатий на клавиатуру
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+  
+    let img = '';
+  
+    if (query.data === 'moreKeks') { // если кот
+      img = 'keks.png';
+    }
+  
+    if (query.data === 'morePes') { // если пёс
+      img = 'pes.png';
+    }
+  
+    if (img) {
+      bot.sendMessage(chatId, img, { // прикрутим клаву
+        reply_markup: {
+          inline_keyboard: keyboard
+        }
+      });
+    } else {
+      bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', {
+        // прикрутим клаву
+        reply_markup: {
+          inline_keyboard: keyboard
+        }
+      });
+    }
+  });  
+
 bot.on('message', (ctx) => {
-    ctx.reply('Send mе any sticker');
+    ctx.reply('You send ' + ctx.query.message, { // прикрутим клаву
+        reply_markup: {
+            inline_keyboard: keyboard
+        }});
 });
 
 const secretPath = `/telegraf/${bot.secretPathComponent()}`;
